4c6f6ad224c58c3dfcfe03af7946ebfee754051b
Ali Farooq <alifarooq122@gmail.com>
2024-07-14 00:48:38 +0500

    fixed change password feature

--- src/app/pages/alumni/account/components/settings/Settings.tsx
+++ src/app/pages/alumni/account/components/settings/Settings.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {ProfileDetails} from './cards/ProfileDetails'
-import {SignInMethod} from './cards/SignInMethod'
+import {SignInMethod} from './cards/ChangePassword'
 // import {ConnectedAccounts} from './cards/ConnectedAccounts'
 // import {EmailPreferences} from './cards/EmailPreferences'
 // import {Notifications} from './cards/Notifications'
