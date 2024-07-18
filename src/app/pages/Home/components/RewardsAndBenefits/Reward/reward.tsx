254dfb1e5fb494e84c3736659ec4d84a9dcd4314
ayezanriaz <ayezanriaz>
2024-07-18 21:35:47 +0500

    Change Testimonial UI Changes

--- src/app/pages/Home/components/RewardsAndBenefits/Reward/reward.tsx
+++ src/app/pages/Home/components/RewardsAndBenefits/Reward/reward.tsx
@@ -3,23 +3,27 @@ import React from 'react';
 
 const Reward: React.FC = () => {
   return (
-    <div className='row mt-15'>
-      <div className='col-lg-12 col-md-12 col-sm-12'>
-        <h1 className='text-css'>
-          Welcome To <span>DHA SUFFA UNIVERSITY</span> Alumni Portal
-        </h1>
+    <div className='container mt-4'>
+      <div className='row text-center mb-4'>
+        <div className='col-12'>
+          <h1 className='text-css'>
+            Welcome To <span>DHA SUFFA UNIVERSITY</span> Alumni Portal
+          </h1>
+        </div>
       </div>
 
-      <div className='col-lg-12 col-md-12 col-sm-12 text-center mt-15'>
-        <div className='video-container'>
-          <iframe
-            src='https://www.youtube.com/embed/AiAQ3dnq6P0?si=tQxPQpW2V0HGTYhi'
-            title='YouTube video player'
-            frameBorder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            referrerPolicy='strict-origin-when-cross-origin'
-            allowFullScreen
-          ></iframe>
+      <div className='row justify-content-center'>
+        <div className='col-lg-8 col-md-10 col-sm-12'>
+          <div className='video-container'>
+            <iframe
+              src='https://www.youtube.com/embed/AiAQ3dnq6P0?si=tQxPQpW2V0HGTYhi'
+              title='YouTube video player'
+              frameBorder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              referrerPolicy='strict-origin-when-cross-origin'
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       </div>
     </div>
