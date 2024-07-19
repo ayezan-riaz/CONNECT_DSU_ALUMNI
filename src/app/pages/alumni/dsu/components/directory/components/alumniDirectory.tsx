b5a8d417b6c152673ee149ac900f5cf9a345ca1e
Ali Farooq <alifarooq122@gmail.com>
2024-07-19 20:42:19 +0500

    changes testimony ui design and fixed few bugs

--- src/app/pages/alumni/dsu/components/directory/components/alumniDirectory.tsx
+++ src/app/pages/alumni/dsu/components/directory/components/alumniDirectory.tsx
@@ -1,108 +1,108 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Modal, Button, Form } from 'react-bootstrap';
-import AlumniDirectoryModal from './alumniDirectoryModal'; // Import the UserModal component
-import { Users } from './alumniDirectoryTypes'; // Import the Directory type
-import './alumniDirectory.css';
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import {Modal, Button, Form} from 'react-bootstrap'
+import AlumniDirectoryModal from './alumniDirectoryModal' // Import the UserModal component
+import {Users} from './alumniDirectoryTypes' // Import the Directory type
+import './alumniDirectory.css'
 
 const AlumniDirectory: React.FC = () => {
-  const [users, setUsers] = useState<Users[]>([]);
-  const [filteredUsers, setFilteredUsers] = useState<Users[]>([]);
-  const [showModal, setShowModal] = useState(false);
-  const [selectedUser, setSelectedUser] = useState<Users | null>(null);
-  const [deleteConfirmation, setDeleteConfirmation] = useState(false);
-  const [userToDelete, setUserToDelete] = useState<Users | null>(null);
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [viewUser, setViewUser] = useState<Users | null>(null); // State for viewing user details
-  const [searchQuery, setSearchQuery] = useState('');
-  const [filterRole, setFilterRole] = useState('All');
-  const Imageurl = 'https://ams-backend-gkxg.onrender.com/api/alumni/';
-  const loggedInUserId = parseInt(localStorage.getItem('sub') || '0', 10);
+  const [users, setUsers] = useState<Users[]>([])
+  const [filteredUsers, setFilteredUsers] = useState<Users[]>([])
+  const [showModal, setShowModal] = useState(false)
+  const [selectedUser, setSelectedUser] = useState<Users | null>(null)
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false)
+  const [userToDelete, setUserToDelete] = useState<Users | null>(null)
+  const [isAdmin, setIsAdmin] = useState(false)
+  const [viewUser, setViewUser] = useState<Users | null>(null) // State for viewing user details
+  const [searchQuery, setSearchQuery] = useState('')
+  const [filterRole, setFilterRole] = useState('All')
+  const Imageurl = 'https://ams-backend-gkxg.onrender.com/api/alumni/'
+  const loggedInUserId = parseInt(localStorage.getItem('sub') || '0', 10)
 
   const fetchUsers = () => {
     axios
       .get<Users[]>('https://ams-backend-gkxg.onrender.com/api/users/directory')
       .then((response) => {
-        setUsers(response.data);
-        setFilteredUsers(response.data); // Initialize filteredUsers with all users
+        setUsers(response.data)
+        setFilteredUsers(response.data) // Initialize filteredUsers with all users
       })
       .catch((error) => {
-        console.error('There was a problem fetching the users data:', error);
-      });
-  };
+        console.error('There was a problem fetching the users data:', error)
+      })
+  }
 
   useEffect(() => {
-    fetchUsers();
-  }, []);
+    fetchUsers()
+  }, [])
 
   useEffect(() => {
-    filterUsers();
-  }, [searchQuery, filterRole, users]);
+    filterUsers()
+  }, [searchQuery, filterRole, users])
 
   const filterUsers = () => {
-    let filtered = users;
+    let filtered = users
 
     if (searchQuery) {
       filtered = filtered.filter((user) =>
         `${user.first_name} ${user.middle_name} ${user.last_name}`
           .toLowerCase()
           .includes(searchQuery.toLowerCase())
-      );
+      )
     }
 
     if (filterRole !== 'All') {
       filtered = filtered.filter((user) =>
         filterRole === 'Admin' ? user.role === 1 : user.role === 2
-      );
+      )
     }
 
     // Exclude users with roleId 1 and logged-in user's data
-    filtered = filtered.filter((user) => user.role !== 1 && user.id !== loggedInUserId);
+    filtered = filtered.filter((user) => user.role !== 1 && user.id !== loggedInUserId)
 
-    setFilteredUsers(filtered);
-  };
+    setFilteredUsers(filtered)
+  }
 
   const openModal = (user: Users | null, admin: boolean) => {
-    setSelectedUser(user);
-    setShowModal(true);
-    setIsAdmin(admin); // Set isAdmin based on the button clicked
-  };
+    setSelectedUser(user)
+    setShowModal(true)
+    setIsAdmin(admin) // Set isAdmin based on the button clicked
+  }
 
   const closeModal = () => {
-    setShowModal(false);
-  };
+    setShowModal(false)
+  }
 
   const openDeleteModal = (user: Users) => {
-    setUserToDelete(user);
-    setDeleteConfirmation(true);
-  };
+    setUserToDelete(user)
+    setDeleteConfirmation(true)
+  }
 
   const closeDeleteModal = () => {
-    setDeleteConfirmation(false);
-    setUserToDelete(null);
-  };
+    setDeleteConfirmation(false)
+    setUserToDelete(null)
+  }
 
   const handleDelete = () => {
     if (userToDelete) {
       axios
         .delete(`https://ams-backend-gkxg.onrender.com/api/users/${userToDelete.id}`)
         .then(() => {
-          setUsers(users.filter((e) => e.id !== userToDelete.id));
-          closeDeleteModal();
+          setUsers(users.filter((e) => e.id !== userToDelete.id))
+          closeDeleteModal()
         })
         .catch((error) => {
-          console.error('There was a problem deleting the user:', error);
-        });
+          console.error('There was a problem deleting the user:', error)
+        })
     }
-  };
+  }
 
   const openViewUserModal = (user: Users) => {
-    setViewUser(user);
-  };
+    setViewUser(user)
+  }
 
   const closeViewUserModal = () => {
-    setViewUser(null);
-  };
+    setViewUser(null)
+  }
 
   return (
     <>
@@ -127,12 +127,14 @@ const AlumniDirectory: React.FC = () => {
         <div className='d-flex gap-5'>
           <div>
             <button className='btn btn-primary des' onClick={() => openModal(null, false)}>
-              Add new user
+              <i className='fa fa-add'></i>
+              User
             </button>
           </div>
           <div>
             <button className='btn btn-primary des' onClick={() => openModal(null, true)}>
-              Add new admin
+              <i className='fa fa-add'></i>
+              Admin
             </button>
           </div>
         </div>
@@ -224,46 +226,61 @@ const AlumniDirectory: React.FC = () => {
         </Modal.Footer>
       </Modal>
       {viewUser && (
-  <Modal show={viewUser !== null} onHide={closeViewUserModal}>
-    <Modal.Header closeButton>
-      <Modal.Title>User Details</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <div className="card">
-        <div className="card-body">
-          <div className="d-flex align-items-center mb-3">
-            <img
-              src={`https://ams-backend-gkxg.onrender.com/alumni/${viewUser.avatar || 'default-avatar.png'}`}
-              alt={`${viewUser.first_name} ${viewUser.last_name}`}
-              className="img-fluid rounded-circle"
-              style={{ width: '100px', height: '100px', objectFit: 'cover' }}
-            />
-            <div className="ms-3">
-              <h5 className="mb-0">{`${viewUser.first_name} ${viewUser.middle_name} ${viewUser.last_name}`}</h5>
+        <Modal show={viewUser !== null} onHide={closeViewUserModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>User Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className='card'>
+              <div className='card-body'>
+                <div className='d-flex align-items-center mb-3'>
+                  <img
+                    src={`https://ams-backend-gkxg.onrender.com/alumni/${
+                      viewUser.avatar || 'default-avatar.png'
+                    }`}
+                    alt={`${viewUser.first_name} ${viewUser.last_name}`}
+                    className='img-fluid rounded-circle'
+                    style={{width: '100px', height: '100px', objectFit: 'cover'}}
+                  />
+                  <div className='ms-3'>
+                    <h5 className='mb-0'>{`${viewUser.first_name} ${viewUser.middle_name} ${viewUser.last_name}`}</h5>
+                  </div>
+                </div>
+                <div className='user-details'>
+                  <p>
+                    <strong>Email:</strong> {viewUser.email}
+                  </p>
+                  <p>
+                    <strong>University Email:</strong> {viewUser.uni_email}
+                  </p>
+                  <p>
+                    <strong>Phone:</strong> {viewUser.phone}
+                  </p>
+                  <p>
+                    <strong>Number of Jobs Posted:</strong> {viewUser.noOfJobsPosted}
+                  </p>
+                  <p>
+                    <strong>Designation:</strong> {viewUser.designation}
+                  </p>
+                  <p>
+                    <strong>Qualification:</strong> {viewUser.qualification}
+                  </p>
+                  <p>
+                    <strong>Company:</strong> {viewUser.company}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="user-details">
-            <p><strong>Email:</strong> {viewUser.email}</p>
-            <p><strong>University Email:</strong> {viewUser.uni_email}</p>
-            <p><strong>Phone:</strong> {viewUser.phone}</p>
-            <p><strong>Number of Jobs Posted:</strong> {viewUser.noOfJobsPosted}</p>
-            <p><strong>Designation:</strong> {viewUser.designation}</p>
-            <p><strong>Qualification:</strong> {viewUser.qualification}</p>
-            <p><strong>Company:</strong> {viewUser.company}</p>
-          </div>
-        </div>
-      </div>
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="secondary" onClick={closeViewUserModal}>
-        Close
-      </Button>
-    </Modal.Footer>
-  </Modal>
-)}
-
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={closeViewUserModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
     </>
-  );
-};
+  )
+}
 
-export default AlumniDirectory;
+export default AlumniDirectory
